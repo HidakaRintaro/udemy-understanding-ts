45c42660815da1bdb769c799ddf50c735771116b
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-12-30 23:53:24 +0900

    fin 53

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-const userName = "Max";
-// userName = "Maximilian";
-let age = 30;
+// const userName = "Max";
+// // userName = "Maximilian";
+// let age = 30;
 
-age = 29;
+// age = 29;
 
 // function add(a: number, b: number) {
 //   let result;
@@ -39,7 +39,7 @@ const activeHobbies = ["Hiking", ...hobbies];
 activeHobbies.push(...hobbies);
 
 const person = {
-  name: "Max",
+  firstName: "Max",
   age: 30,
 };
 
@@ -55,3 +55,9 @@ const add = (...numbers: number[]) => {
 
 const addedNumbers = add(5, 10, 2, 3.7);
 console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
