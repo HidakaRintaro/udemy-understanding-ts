05ce5854805ef9e87569a64a700ddd9a203b1dc3
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-12-30 23:35:47 +0900

    fin 48

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+const userName = "Max";
+// userName = "Maximilian";
+let age = 30;
+
+age = 29;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+// if (age >= 20) {
+//   let isAdult = true;
+// }
+
+// console.log(isAdult);
+
+// console.log(result);
