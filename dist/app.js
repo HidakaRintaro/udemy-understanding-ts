5608abc08ef792a191e1d66d8f5a2abce37d975a
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-12-31 16:15:59 +0900

    fin 87

--- dist/app.js
+++ dist/app.js
@@ -10,58 +10,6 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployeeInformation(emp) {
-    console.log(emp.name);
-    if ("privileges" in emp) {
-        console.log("Privileges: " + emp.privileges);
-    }
-    if ("startDate" in emp) {
-        console.log("Start Date: " + emp.startDate);
-    }
-}
-printEmployeeInformation({ name: "Manu", startDate: new Date() });
-class Car {
-    drive() {
-        console.log("運転中...");
-    }
-}
-class Truck {
-    drive() {
-        console.log("トラックを運転中...");
-    }
-    loadCargo(amount) {
-        console.log("荷物を載せています... " + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log("移動速度: " + speed);
-}
-moveAnimal({ type: "bird", flyingSpeed: 10 });
-const userInputElement = document.getElementById("user-input");
-if (userInputElement) {
-    userInputElement.value = "こんにちは";
-}
-const errorBag = {
-    email: "正しいメールアドレスではありません",
-    username: "ユーザ名に記号を含めることはできません",
-};
+const result = add("Hello", " TypeScript");
+result.split(" ");
 //# sourceMappingURL=app.js.map
