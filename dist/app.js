1f13cbdfebcf261930589f4c9dffe20846ce0d61
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-12-31 15:55:47 +0900

    fin 84

--- dist/app.js
+++ dist/app.js
@@ -43,4 +43,17 @@ function useVehicle(vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log("移動速度: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
 //# sourceMappingURL=app.js.map
