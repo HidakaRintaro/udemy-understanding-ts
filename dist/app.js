abfda2fd2fde40008024203a201d64c3c1b1e3ff
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-12-31 19:53:50 +0900

    fin 96

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,12 @@ function merge(objA, objB) {
 }
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj.age);
+function countAndDescribe(element) {
+    let descriptionText = "値がありません";
+    if (element.length > 0) {
+        descriptionText = "値は" + element.length + "個です";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(["Sports", "Cooking"]));
 //# sourceMappingURL=app.js.map
